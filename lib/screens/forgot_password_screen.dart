93445d2dd1b3e85bffd49da3f9951150de94d852
Nisasu <bozkurtnisasu@hotmail.com>
2023-07-05 00:06:48 +0300

    conf

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({Key? key}) : super(key: key);
@@ -29,7 +30,11 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
         builder: (context) {
           return AlertDialog(
             content:
+<<<<<<< HEAD
             Text('Şifre sıfırlama bağlantısı mail adresine gönderildi!'),
+=======
+                Text('Şifre sıfırlama bağlantısı mail adresine gönderildi!'),
+>>>>>>> 1d1104e (şifremi unuttum ekran tasarımı yapıldı.)
             title: Text('Uyarı'),
             actions: [
               TextButton(
@@ -67,6 +72,10 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
     }
   }
 
+  void _goToLogin() {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,6 +90,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
             end: Alignment.bottomCenter,
           ),
         ),
+<<<<<<< HEAD
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
@@ -142,6 +152,109 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
               ],
             ),
           ),
+=======
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20 * MediaQuery.of(context).size.height / 100 +
+                  20, // 20 cm kadar aşağı indirildi
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: _goToLogin,
+                  icon: Icon(
+                    FontAwesomeIcons.caretLeft,
+                    color: Color.fromRGBO(135, 142, 205, 1),
+                  ),
+                ),
+                SizedBox(
+                  width: 55,
+                ),
+                Text(
+                  'Şifre Sıfırlama',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 32,
+                      color: Color.fromRGBO(135, 142, 205, 1)),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Şifrenizi Sıfırlamak için E-Posta adresinizi girin!',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color.fromRGBO(135, 142, 205, 1),
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'E-posta',
+                            border: InputBorder.none,
+                            labelStyle: TextStyle(
+                              color: Color.fromRGBO(135, 142, 205, 1),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                              vertical: 14.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Image.asset(
+                        'lib/assets/aPngtreeahand_drawn_cute_cat_reading_4361091.png',
+                        height: 90,
+                        width: 90,
+                      ),
+                      SizedBox(height: 0.0),
+                      ElevatedButton(
+                        onPressed: resetPassword,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Color.fromRGBO(135, 142, 205, 1),
+                          ),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          padding: MaterialStateProperty.all(
+                            EdgeInsets.symmetric(
+                                vertical: 18.0, horizontal: 40.0),
+                          ),
+                        ),
+                        child: Text('Şifremi Sıfırla'),
+                      ),
+                      if (_isEmailSent)
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          child: Text(
+                            'Şifre sıfırlama bağlantısı mail adresinize gönderildi!',
+                            style: TextStyle(color: Colors.green),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+>>>>>>> 1d1104e (şifremi unuttum ekran tasarımı yapıldı.)
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -48,7 +48,11 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
+<<<<<<< HEAD
       padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+=======
+      padding: EdgeInsets.all(50),
+>>>>>>> 1d1104e (şifremi unuttum ekran tasarımı yapıldı.)
       decoration: BoxDecoration(
         gradient: RadialGradient(
           colors: [
@@ -86,7 +90,11 @@ class _RegisterPageState extends State<RegisterPage> {
                 ],
               ),
               SizedBox(
+<<<<<<< HEAD
                 height: 50,
+=======
+                height: 150,
+>>>>>>> 1d1104e (şifremi unuttum ekran tasarımı yapıldı.)
               ),
               Container(
                 height: 68,
