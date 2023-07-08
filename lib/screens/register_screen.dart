ab4dedaeadb7d40f30421217c008dad242495447
Eren Malkoç <malkoceren24@gmail.com>
2023-07-08 18:19:05 +0300

    ..

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -30,12 +30,12 @@ class _RegisterPageState extends State<RegisterPage> {
           context: context,
           builder: (BuildContext context) {
             return AlertDialog(
-              title: const Text('Username Exists'),
-              content: const Text(
+              title: Text('Username Exists'),
+              content: Text(
                   'The username already exists. Please choose a different username.'),
               actions: <Widget>[
                 TextButton(
-                  child: const Text('OK'),
+                  child: Text('OK'),
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
@@ -62,11 +62,9 @@ class _RegisterPageState extends State<RegisterPage> {
             context,
             MaterialPageRoute(
                 builder: (context) => LoginPage(
-
-                  email: _emailController.text,
-                  password: _passwordController.text,
-                )));
-
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    )));
       }
     } catch (e) {
       setState(() {
@@ -114,15 +112,13 @@ class _RegisterPageState extends State<RegisterPage> {
     Future<void> addUser() {
       return users
           .add({
-
-        'username': _userNameController.text,
-        'currentPoint': 0,
-        'lastReadDate': DateTime.now(),
-        'totalTime': 0,
-        'userId': _auth.currentUser!.uid,
-        'donationCount': 0
-      })
-
+            'username': _userNameController.text,
+            'currentPoint': 0,
+            'lastReadDate': DateTime.now(),
+            'totalTime': 0,
+            'userId': _auth.currentUser!.uid,
+            'donationCount': 0
+          })
           .then((value) => print("User Added"))
           .catchError((error) => print("Failed to add user: $error"));
     }
@@ -132,10 +128,8 @@ class _RegisterPageState extends State<RegisterPage> {
         child: Container(
           height: 1400,
           width: 600,
-
           padding: EdgeInsets.all(50),
           decoration: BoxDecoration(
-
             gradient: RadialGradient(
               colors: [
                 Color.fromRGBO(185, 187, 223, 1),
@@ -147,65 +141,108 @@ class _RegisterPageState extends State<RegisterPage> {
             ),
           ),
           child: SingleChildScrollView(
-
-            child: Column(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: _goToLogin,
+                    icon: Icon(FontAwesomeIcons.caretLeft),
+                    color: Color.fromRGBO(135, 142, 205, 1),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(height: 30,),
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: _goToLogin,
-                        icon: Icon(FontAwesomeIcons.caretLeft),
-                        color: Color.fromRGBO(135, 142, 205, 1),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 50,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Kayıt Ol',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          fontSize: 32,
-                          color: Color.fromRGBO(135, 142, 205, 1),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 50),
-                  Container(
-                    height: 68,
-                    width: 320,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 0.0,
-                          blurRadius: 1.0,
-                          offset: Offset(0, 3), // horizontal, vertical offset
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.0),
+                  Text(
+                    'Kayıt Ol',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 32,
+                      color: Color.fromRGBO(135, 142, 205, 1),
                     ),
-                    child: TextFormField(
-                      style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      controller: _userNameController,
-                      decoration: InputDecoration(
-                        labelText: 'İsim',
-                        labelStyle:
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              Container(
+                height: 68,
+                width: 320,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 0.0,
+                      blurRadius: 1.0,
+                      offset: Offset(0, 3), // horizontal, vertical offset
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: TextFormField(
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  controller: _userNameController,
+                  decoration: InputDecoration(
+                    labelText: 'İsim',
+                    labelStyle:
                         TextStyle(color: Color.fromRGBO(170, 170, 170, 1)),
-                        border: InputBorder.none,
-                        contentPadding:
+                    border: InputBorder.none,
+                    contentPadding:
                         EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                      ),
-
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                height: 68,
+                width: 320,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 0.0,
+                      blurRadius: 1.0,
+                      offset: Offset(0, 3), // horizontal, vertical offset
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: TextFormField(
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'E-posta',
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(170, 170, 170, 1)),
+                    border: InputBorder.none,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Container(
+                height: 68,
+                width: 320,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 0.0,
+                      blurRadius: 1.0,
+                      offset: Offset(0, 3),
                     ),
                   ],
                   color: Colors.white,
@@ -213,9 +250,8 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
                 child: TextFormField(
                   controller: _passwordController,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                  decoration: const InputDecoration(
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
                     labelText: 'Şifre',
                     labelStyle:
                         TextStyle(color: Color.fromRGBO(170, 170, 170, 1)),
@@ -224,111 +260,42 @@ class _RegisterPageState extends State<RegisterPage> {
                         EdgeInsets.symmetric(horizontal: 15, vertical: 30),
                     floatingLabelBehavior: FloatingLabelBehavior.never,
                   ),
-
-                  SizedBox(height: 30),
-                  Container(
-                    height: 68,
-                    width: 320,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 0.0,
-                          blurRadius: 1.0,
-                          offset: Offset(0, 3), // horizontal, vertical offset
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: TextFormField(
-                      style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'E-posta',
-                        labelStyle:
-                        TextStyle(color: Color.fromRGBO(170, 170, 170, 1)),
-                        border: InputBorder.none,
-                        contentPadding:
-                        EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                      ),
-                    ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Column(
+                children: [
+                  Image.asset(
+                    'lib/assets/aPngtreeahand_drawn_cute_cat_reading_4361091.png',
+                    height: 90,
+                    width: 90,
                   ),
-                  SizedBox(height: 30.0),
-                  Container(
-                    height: 68,
-                    width: 320,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 0.0,
-                          blurRadius: 1.0,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: TextFormField(
-                      controller: _passwordController,
-                      style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      decoration: InputDecoration(
-                        labelText: 'Şifre',
-                        labelStyle:
-                        TextStyle(color: Color.fromRGBO(170, 170, 170, 1)),
-                        border: InputBorder.none,
-                        contentPadding:
-                        EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Color.fromRGBO(135, 142, 205, 1)),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      padding: MaterialStateProperty.all(
+                        EdgeInsets.symmetric(vertical: 18.0, horizontal: 40.0),
                       ),
-                    ),
-                  ),
-                  SizedBox(height: 16.0),
-                  Column(
-                    children: [
-                      Image.asset(
-                        'lib/assets/aPngtreeahand_drawn_cute_cat_reading_4361091.png',
-                        height: 90,
-                        width: 90,
+                      textStyle: MaterialStateProperty.all(
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                       ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              Color.fromRGBO(135, 142, 205, 1)),
-                          foregroundColor:
-                          MaterialStateProperty.all(Colors.white),
-                          padding: MaterialStateProperty.all(
-                            EdgeInsets.symmetric(
-                                vertical: 18.0, horizontal: 40.0),
-                          ),
-                          textStyle: MaterialStateProperty.all(
-                            TextStyle(
-                                fontSize: 20.0, fontWeight: FontWeight.bold),
-                          ),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(51.16),
-                            ),
-                          ),
-
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(51.16),
                         ),
                       ),
-
                     ),
                     onPressed: () async {
                       await _register();
                       addUser();
                     },
-                    child: const Text('Kayıt Ol'),
+                    child: Text('Kayıt Ol'),
                   ),
                 ],
               ),
             ]),
-
           ),
         ),
       ),
