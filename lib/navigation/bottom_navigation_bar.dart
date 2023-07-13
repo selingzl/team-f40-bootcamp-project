67cef4a43ba0db3674b724fce5ba8078d1882d42
Eren Malkoç <malkoceren24@gmail.com>
2023-07-13 22:28:47 +0300

    lib screen and ui updated,api reused,some new algorithms and welcome screen added,new font set added

--- lib/navigation/bottom_navigation_bar.dart
+++ lib/navigation/bottom_navigation_bar.dart
@@ -33,7 +33,7 @@ class _BottomNavigationBarPageState extends State<BottomNavigationBarPage> {
   }
 
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
   /*static const List<Widget> _widgetOptions = <Widget>[
     TimerPage(bookName: bookTitle),
@@ -53,14 +53,13 @@ class _BottomNavigationBarPageState extends State<BottomNavigationBarPage> {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          gradient: RadialGradient(
+          gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter,
             colors: [
+              Color.fromRGBO(185, 187, 223, 1),
               Color.fromRGBO(223, 244, 243, 1),
-              Color.fromRGBO(218, 228, 238, 1),
               Color.fromRGBO(185, 187, 223, 1),
             ],
-            radius: 1.65,
-            center: Alignment.topLeft,
+
           ),
         ),
         child: Stack(children: [_widgetOptions[_selectedIndex]]),
